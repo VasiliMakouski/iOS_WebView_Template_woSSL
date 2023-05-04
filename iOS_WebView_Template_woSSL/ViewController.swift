787ce7c6e66ec95624e6b3d14fa40dc8686cb790
Vasili.Makouski <vasili@makouski.com>
2023-05-04 21:48:17 +0300

    Initial Commit

---
+++ iOS_WebView_Template_woSSL/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iOS_WebView_Template_woSSL
+//
+//  Created by Vasili Makouski on 4.05.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
